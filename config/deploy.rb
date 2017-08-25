7dae5547d82b74e0fb2c484d96df85ab5135add4
WiseBell <neoswirl@gmail.com>
2017-08-25 15:34:11 +0900

    puma 설정 추

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,34 @@
 # config valid only for current version of Capistrano
 lock "3.9.0"
 
-set :application, "MinervaWebApp"
-set :repo_url, "git@github.com:devglade/minerva-webapp.git"
-
 set :ssh_options, {
   forward_agent: true,
   auth_methods: ["publickey"],
   keys: ["#{Dir.home}/Key/minerva-key.pem"]
 }
 
-set :deploy_to, '/var/www/apps/minerva'
+set :application,     "MinervaWebApp"
+set :user,            'ubuntu'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :repo_url,        "git@github.com:devglade/minerva-webapp.git"
+
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       '/var/www/apps/minerva'
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -42,3 +60,52 @@ set :deploy_to, '/var/www/apps/minerva'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
