0bcbbea93e08af3d5303e4bab931fe7a846ea683
WiseBell <neoswirl@gmail.com>
2017-08-31 13:22:06 +0900

    assets_roles 설정 추

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-
+set :assets_roles, [:web, :app]
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
