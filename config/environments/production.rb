0c82d0c8c2ce6d6047a1f23150340a9d2aa5a078
Jooyeon Song <wndus85@gmail.com>
2017-09-03 23:28:26 +0900

    production.rb 수정.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,14 +21,18 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -89,9 +93,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.read_encrypted_secrets = true
-
-  config.serve_static_assets = true
-  config.public_file_server.enabled = true
-  config.assets.digest = true
+  # config.serve_static_assets = true
+  # config.public_file_server.enabled = true
 end
