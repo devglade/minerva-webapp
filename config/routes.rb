845a78176a85c27b6abc924984613233843705b6
Jooyeon Song <wndus85@gmail.com>
2017-09-04 23:07:55 +0900

    root controller 추가.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  authenticated :user do
+    root to: 'home#index', as: :home
+  end
+  root to: 'root#index'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
-  get   'home/index'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
