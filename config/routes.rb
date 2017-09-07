fa780d5ef65881b2a7d208a71d2b933220cce0bd
WiseBell <neoswirl@gmail.com>
2017-09-07 16:16:44 +0900

    템플릿 적용

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  authenticated :user do
-    root to: 'home#index', as: :home
-  end
-  root to: 'root#index'
+
+  #authenticated :user do
+  #  root to: 'home#index', as: :home
+  # end
+  # root to: 'root#index'
 
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  get   'home/index'
+
 end
