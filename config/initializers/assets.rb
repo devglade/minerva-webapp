17a1ff495c8849e179cf9fe5a895374d5f114073
neoswirl <neoswirl@gmail.com>
2017-09-12 22:48:11 +0900

    asset 리소스 파일 지정 추가

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,13 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( main.css font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js skel.min.js util.js ie/respond.min.js main.js )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff2 fontawesome-webfont.woff fontawesome-webfont.ttf fontawesome-webfont.svg )
+Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.png )
+Rails.application.config.assets.precompile += %w( *.sass )
+
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
