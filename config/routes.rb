332ce2ab1b409bf1761ae8fffb54c93edfd97218
Jooyeon <wndus85@gmail.com>
2017-09-18 22:23:48 +0900

    메인 화면 제작 전 코드 정리...

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-
-  resources :retrospects
-  #authenticated :user do
-  #  root to: 'home#index', as: :home
-  # end
-  # root to: 'root#index'
+  authenticated :user do
+    root to: 'home#index.html.erb', as: :home
+  end
+  root to: 'root#index.html.erb'
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    mailer: 'users/mailer',
-    shared: 'users/shared',
-    unlocks: 'users/unlocks'
+      sessions: 'users/sessions',
+      confirmations: 'users/confirmations',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations',
+      mailer: 'users/mailer',
+      shared: 'users/shared',
+      unlocks: 'users/unlocks'
   }
-
-
+  resources :retrospects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
-  get   'home/index'
-
 end
