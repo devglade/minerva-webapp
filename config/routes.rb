c43ee04aff68b6ae08f0f6b00a8e0beb09ea8a88
WiseBell <neoswirl@gmail.com>
2017-09-18 19:36:02 +0900

    rails generate devise:controllers users 명령을 통해 커스터마이즈를 위한 컨트롤러들 생성 및 라우트에 devise 컨트롤러들 적용

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,16 @@ Rails.application.routes.draw do
   # end
   # root to: 'root#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    mailer: 'users/mailer',
+    shared: 'users/shared',
+    unlocks: 'users/unlocks'
+  }
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
