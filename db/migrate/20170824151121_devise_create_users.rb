6b72df4ab2162b053f15568bba8a2b2f34987809
WiseBell <neoswirl@gmail.com>
2017-09-18 19:36:33 +0900

    users 데이터베이스에 사용자 이름 필드 추

--- db/migrate/20170824151121_devise_create_users.rb
+++ db/migrate/20170824151121_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170918065739) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
