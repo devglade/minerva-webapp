46d6ebb02834037787b6a6b10a0ebaa39fee9c89
WiseBell <neoswirl@gmail.com>
2017-09-19 13:33:48 +0900

    포스트 모델 및 컨트롤러 생성

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918065739) do
+ActiveRecord::Schema.define(version: 20170919025324) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "retrospects", force: :cascade do |t|
     t.string "name", limit: 500
     t.text "description"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_retrospects_on_user_id"
@@ -39,4 +50,6 @@ ActiveRecord::Schema.define(version: 20170918065739) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "retrospects", "users"
 end
