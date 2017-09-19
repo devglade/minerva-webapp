78220cb6ac09f44e3b5e8c2ca6dd1958c7b9f4c6
WiseBell <neoswirl@gmail.com>
2017-09-19 13:34:44 +0900

    포스트 레이아웃을 위한 에셋 추가

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w( main.css font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( main.css font-awesome.min.css posts.sass )
 Rails.application.config.assets.precompile += %w( jquery.min.js skel.min.js util.js ie/respond.min.js main.js )
 Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff2 fontawesome-webfont.woff fontawesome-webfont.ttf fontawesome-webfont.svg )
 Rails.application.config.assets.precompile += %w( *.jpg )
