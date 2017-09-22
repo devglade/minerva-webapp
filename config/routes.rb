f0063e62ee0e42d3b0d7b09a17e0c4b312af1b3d
WiseBell <neoswirl@gmail.com>
2017-09-23 00:45:24 +0900

    ActionCable.server 마운트

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server => '/cable'
 end
