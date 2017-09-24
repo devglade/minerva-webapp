e0a7b3acf70f1d0bdef84c8312aa0758e23677fb
Jooyeon <wndus85@gmail.com>
2017-09-25 00:10:48 +0900

    post controller test케이스 추가.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "로그인 안 한 사람은 포스트글을 볼 수 없다" do
+    post = posts(:one)
+    get posts_url(post)
+    assert_response 401
+  end
+
+  test "로그인 한 사람은 포스트를 볼 수 있다" do
+    assert true
+  end
+
+  test "자신의 글은 삭제 할 수 있다." do
+    assert true
+  end
+
+  test "다른 사람의 글은 삭제 할 수 없다." do
+    assert true
+  end
 end
