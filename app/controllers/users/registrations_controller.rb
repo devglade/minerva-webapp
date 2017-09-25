3f295941a046096a5f4c6c8907e985e9fac18cb9
WiseBell <neoswirl@gmail.com>
2017-09-25 13:37:32 +0900

    사용자 계정 생성 및 업데이트 시 사용자 이름이 저장되도록 수정.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -57,4 +57,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+    def sign_up_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
 end
