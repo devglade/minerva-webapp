5615c34f13da0525e786dd91fea9dc99ae61f779
Jooyeon <wndus85@gmail.com>
2017-09-27 22:05:05 +0900

    modal 안보였던문제 수정.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
