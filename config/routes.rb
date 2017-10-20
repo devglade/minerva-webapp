cbbf675afbb516988837e5e0af9c09164d1b4fc8
WiseBell <neoswirl@gmail.com>
2017-10-20 15:36:40 +0900

    posts 리소스에 vote 관련 경로 추가

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,12 @@ Rails.application.routes.draw do
       unlocks: 'users/unlocks'
   }
   resources :retrospects
-  resources :posts
+  resources :posts do
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
