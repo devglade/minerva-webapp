23399e3a75f89bede44f28be47f58c4ee35f447a
Jooyeon <wndus85@gmail.com>
2017-11-05 17:47:19 +0900

    assets.rb 수정

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,9 +9,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( *.jpg )
-Rails.application.config.assets.precompile += %w( *.png )
-Rails.application.config.assets.precompile += %w( *.sass )
+# application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
