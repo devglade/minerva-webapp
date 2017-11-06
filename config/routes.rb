74d19609f7062874e42212e4312bb6a90e8e749a
WiseBell <neoswirl@gmail.com>
2017-11-06 22:49:55 +0900

    카카오 redirect_path 버그가 있는것으로 보여 url match 적용

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   end
 
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  match '/auth/kakao/callback' => '/users/auth/kakao/callback'
 
   controller :help_center do
     get '/about', action: :about
