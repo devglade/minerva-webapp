58180b50f2e9eba18d943b7d1c474a98aaa9cd30
WiseBell <neoswirl@gmail.com>
2017-11-06 17:13:37 +0900

    confirmable 옵션 일단 제거.. 차후 적용..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :identities, dependent: :destroy
