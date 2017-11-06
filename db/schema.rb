595bb3c5ff01aee34fa9f3090b547bbccf4030c5
WiseBell <neoswirl@gmail.com>
2017-11-06 16:22:06 +0900

    confirmable 관련 스키마 변경 적용

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101100710) do
+ActiveRecord::Schema.define(version: 20171106071903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
