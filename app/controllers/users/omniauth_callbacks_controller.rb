b3ab26dfd9530cde4d6d11404f0849b357b133b9
WiseBell <neoswirl@gmail.com>
2017-11-10 16:17:19 +0900

    Could not authenticate you from GoogleOauth2 because “Invalid credentials” 오류 해결을 위해 skip_before_action을 추가

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
