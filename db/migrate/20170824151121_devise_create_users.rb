688c1be28c1bd87f9846cc96f5d865e99f744ec7
Jooyeon <wndus85@gmail.com>
2017-11-12 21:44:07 +0900

    current_sign_in_ip, last_sign_in_ip 를 network address type 으로 변경

--- db/migrate/20170824151121_devise_create_users.rb
+++ db/migrate/20170824151121_devise_create_users.rb
@@ -18,8 +18,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
 
-      t.string     :current_sign_in_ip
-      t.string     :last_sign_in_ip
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
 
       ## Confirmable
