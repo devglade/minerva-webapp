9e84dad4d847a778dbc0d5fd74a7afc443a59cfe
Jooyeon <wndus85@gmail.com>
2017-11-26 22:53:44 +0900

    "should create retrospect" 테스트 코드 추가.

--- test/controllers/retrospects_controller_test.rb
+++ test/controllers/retrospects_controller_test.rb
@@ -17,14 +17,15 @@ class RetrospectsControllerTest < ActionDispatch::IntegrationTest
     retrospect = assigns(:retrospect)
     assert_not_nil retrospect
   end
-  #
-  # test "should create retrospect" do
-  #   assert_difference('Retrospect.count') do
-  #     post retrospects_url, params: {retrospect: {description: @retrospect.description, title: @retrospect.title, user_id: @retrospect.user_id}}
-  #   end
-  #
-  #   assert_redirected_to retrospect_url(Retrospect.last)
-  # end
+
+  test "should create retrospect" do
+    assert_difference 'Retrospect.count' do
+      post retrospects_url, params: {retrospect: attributes_for(:retrospect)}, xhr: true
+    end
+
+    assert_response :success
+    assert_template 'retrospects/create'
+  end
   #
   # test "should show retrospect" do
   #   get retrospect_url(@retrospect)
