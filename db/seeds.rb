c3bfbfd6f31b1fffe87dd5c42f66cef64db56706
Jooyeon <wndus85@gmail.com>
2017-11-26 18:31:21 +0900

    user 에 confirmed_at: Time.now 추가(자꾸 리다이렉트 뜨는거 불편해서...-_ㅜ)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-user_1 = User.create(email: 'neoswirl@gmail.com', password: 'password', name: '노현종')
-user_2 = User.create(email: 'wndus85@gmail.com', password: 'password', name: '송주연')
+user_1 = User.create(email: 'neoswirl@gmail.com', password: 'password', name: '노현종', confirmed_at: Time.now)
+user_2 = User.create(email: 'wndus85@gmail.com', password: 'password', name: '송주연', confirmed_at: Time.now)
 #
 retrospect = Retrospect.create title: '데브글레이드 회고', description: 'RetroSpin 개발 회고', user_id: user_1.id
 spin = Spin.create status: 1, title: '1차개발 회고', summary: '처음으로 해본 프로젝트에 대해 회고해봅시다.', retrospect_id: retrospect.id, user_id: user_1.id
