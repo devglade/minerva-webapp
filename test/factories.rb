a06f48c39cfcd53df00f89b78a5bf5655dab3649
Jooyeon <wndus85@gmail.com>
2017-11-26 18:21:59 +0900

    factories.rb 추가하고 fixtures 파일들 제거.

---
+++ test/factories.rb
@@ -0,0 +1,29 @@
+FactoryBot.define do
+  factory :user do
+    name {Forgery(:internet).user_name}
+    email {Forgery(:internet).email_address}
+    password {Forgery(:basic).password({at_least: 8})}
+    password_confirmation {|u| u.password}
+    confirmed_at Time.now
+  end
+
+  factory :retrospect do
+    title {Forgery(:lorem_ipsum).words(20)}
+    description {Forgery(:lorem_ipsum).words(20)}
+    user
+  end
+
+  factory :spin do
+    status 1
+    title {Forgery(:lorem_ipsum).words(20)}
+    summary {Forgery(:lorem_ipsum).words(20)}
+    retrospect
+    user
+  end
+
+  factory :post do
+    content {Forgery(:lorem_ipsum).words(20)}
+    user
+    spin
+  end
+end
