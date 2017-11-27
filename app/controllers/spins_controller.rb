95c639853b968457ee3bdaa802166f431d06c917
Jooyeon <wndus85@gmail.com>
2017-11-27 23:35:53 +0900

    spin create 생성 안되던 문제 수정.

--- app/controllers/spins_controller.rb
+++ app/controllers/spins_controller.rb
@@ -3,78 +3,58 @@ class SpinsController < ApplicationController
   before_action :set_retrospect
   before_action :authenticate_user!
 
-  # GET /spins
-  # GET /spins.json
   def index
     @spins = Spin.where(retrospect_id: params[:retrospect_id]).order('created_at DESC')
   end
 
-  # GET /spins/1
-  # GET /spins/1.json
   def show
   end
 
-  # GET /spins/new
   def new
     @spin = Spin.new
   end
 
-  # GET /spins/1/edit
   def edit
   end
 
-  # POST /spins
-  # POST /spins.json
   def create
-    @spin = Spin.new(spin_params)
-
-    respond_to do |format|
-      if @spin.save
-        format.json { render :show, status: :created, location: @spin }
-        format.js { render js: '$(".modal").modal("hide");' }
-      else
-        format.html { render :index }
-        format.json { render json: @spin.errors, status: :unprocessable_entity }
-      end
-    end
+    @retrospect = Retrospect.find(params[:retrospect_id])
+    @spin = @retrospect.spins.build(spin_params.merge(user: current_user))
+    @spin.save
   end
 
-  # PATCH/PUT /spins/1
-  # PATCH/PUT /spins/1.json
   def update
     respond_to do |format|
       if @spin.update(spin_params)
-        format.json { render :show, status: :ok, location: @spin }
-        format.js { render js: '$(".modal").modal("hide");' }
+        format.json {render :show, status: :ok, location: @spin}
+        format.js {render js: '$(".modal").modal("hide");'}
       else
-        format.html { render :edit }
-        format.json { render json: @spin.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @spin.errors, status: :unprocessable_entity}
       end
     end
   end
 
-  # DELETE /spins/1
-  # DELETE /spins/1.json
   def destroy
     @spin.destroy
     respond_to do |format|
       # format.html { redirect_to spins_url, notice: 'Spin was successfully destroyed.' }
-      format.json { head :no_content }
+      format.json {head :no_content}
     end
   end
 
   private
-    def set_retrospect
-        @retrospect = Retrospect.find(params[:retrospect_id])
-    end
+  def set_retrospect
+    @retrospect = Retrospect.find(params[:retrospect_id])
+  end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_spin
-      @spin = Spin.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_spin
+    @spin = Spin.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def spin_params
-      params.require(:spin).permit(:status, :title, :summary)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def spin_params
+    params.require(:spin).permit(:status, :title, :summary)
+  end
 end
