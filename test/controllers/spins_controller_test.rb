97c0bd9e2edfe7c61b8146cf0142e4cfb55ccbb6
WiseBell <neoswirl@gmail.com>
2017-11-28 13:47:17 +0900

    테스트 코드 수정하여 테스트 패스

--- test/controllers/spins_controller_test.rb
+++ test/controllers/spins_controller_test.rb
@@ -8,43 +8,44 @@ class SpinsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get retrospect_spin_path @retrospect, @spin
+    get retrospect_spin_url @retrospect, @spin
     assert_response :success
   end
 
   test "should get new" do
-    get new_retrospect_spin @retrospect, @spin
+    get new_retrospect_spin_url(@retrospect, @spin), xhr: true
+    assert_response :success
+    spin = assigns(:spin)
+    assert_not_nil spin
+  end
+
+  test "should create spin" do
+    assert_difference('Spin.count') do
+      post retrospect_spins_url @retrospect, @spin, params: { spin: { status: @spin.status, summary: @spin.summary, title: @spin.title } }
+    end
     assert_response :success
   end
-  #
-  # test "should create spin" do
-  #   assert_difference('Spin.count') do
-  #     post spins_url, params: { spin: { status: @spin.status, summary: @spin.summary, title: @spin.title } }
-  #   end
-  #
-  #   assert_redirected_to spin_url(Spin.last)
-  # end
-  #
-  # test "should show spin" do
-  #   get spin_url(@spin)
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get edit_spin_url(@spin)
-  #   assert_response :success
-  # end
-  #
-  # test "should update spin" do
-  #   patch spin_url(@spin), params: { spin: { status: @spin.status, summary: @spin.summary, title: @spin.title } }
-  #   assert_redirected_to spin_url(@spin)
-  # end
-  #
-  # test "should destroy spin" do
-  #   assert_difference('Spin.count', -1) do
-  #     delete spin_url(@spin)
-  #   end
-  #
-  #   assert_redirected_to spins_url
-  # end
+
+  test "should show spin" do
+    get retrospect_spin_url @retrospect, @spin
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_retrospect_spin_url(@retrospect, @spin, params: { spin: { status: @spin.status, summary: @spin.summary, title: @spin.title } }), xhr: true
+    assert_response :success
+  end
+
+  test "should update spin" do
+    patch retrospect_spin_url @retrospect, @spin, params: { spin: { status: @spin.status, summary: @spin.summary, title: "changed" } }, format: :js
+    assert_response :success
+    assert_equal "changed", Spin.find(@spin.id).title
+  end
+
+  test "should destroy spin" do
+    assert_difference('Spin.count', -1) do
+      delete retrospect_spin_url(@retrospect, @spin, format: :json), xhr: true
+    end
+    assert_response :success
+  end
 end
