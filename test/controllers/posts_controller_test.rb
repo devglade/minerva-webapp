69f31f197cdb7097d239d571d829db727115160d
Jooyeon <wndus85@gmail.com>
2017-12-19 00:00:39 +0900

    post_controller 기본 테스트 코드 추가 및 show.html.erb 에러 있던것 수정.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -13,4 +13,34 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get new" do
+    get new_retrospect_spin_post_url(@retrospect, @spin), xhr: true
+    assert_response :success
+    post = assigns(:post)
+    assert_not_nil post
+  end
+
+  test "should create post" do
+    assert_difference('Post.count') do
+      post retrospect_spin_posts_url(@retrospect, @spin), params: {post: {content: "test"}}, xhr: true
+    end
+    assert_response :success
+  end
+  test "should show post" do
+    get retrospect_spin_post_url @retrospect, @spin, @post
+    assert_response :success
+  end
+
+  test "should update spin" do
+    patch retrospect_spin_post_url @retrospect, @spin, @post, params: {post: {content: "changed"}}, format: :js
+    assert_response :success
+    assert_equal "changed", Post.find(@post.id).content
+  end
+
+  test "should destroy spin" do
+    assert_difference('Post.count', -1) do
+      delete retrospect_spin_post_path(@retrospect, @spin, @post), xhr: true
+    end
+    assert_response :success
+  end
 end
