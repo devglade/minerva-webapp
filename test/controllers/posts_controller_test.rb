18bde9b6d3ae6d3525a9a9f7f340383466c8cb57
Jooyeon <wndus85@gmail.com>
2017-12-18 23:50:26 +0900

    post test 코드 추가.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,21 +1,16 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  #
-  # test "로그인 안 한 사람은 포스트글을 볼 수 없다" do
-  #   get posts_url(@post)
-  #   assert_response 401
-  # end
-  #
-  # test "로그인 한 사람은 포스트를 볼 수 있다" do
-  #   assert true
-  # end
-  #
-  # test "자신의 글은 삭제 할 수 있다." do
-  #   assert true
-  # end
-  #
-  # test "다른 사람의 글은 삭제 할 수 없다." do
-  #   assert true
-  # end
+  setup do
+    sign_in @user1
+    @retrospect = create(:retrospect)
+    @spin = create(:spin)
+    @post = create(:post)
+  end
+
+  test "should get index" do
+    get retrospect_spin_posts_url @retrospect, @spin
+    assert_response :success
+  end
+
 end
