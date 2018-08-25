3be51a96c660e8c285e1cb4bd1fa0bae53a2452e
Jooyeon Song <wndus85@gmail.com>
2018-08-25 15:42:54 +0900

    seeds 수정및 position default 값은 1로 변경

--- db/migrate/20170919025324_create_posts.rb
+++ db/migrate/20170919025324_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
       t.belongs_to :user, null: false
       t.belongs_to :spin, null: false
       t.belongs_to :section
-      t.integer :position
+      t.integer :position, default: 1, null: false
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ project = Project.create title: '데브글레이드 회고', description: 'Retro
 project = Project.create title: '나만의 회고', description: 'RetroSpin 개발 회고', user_id: user_2.id, space_id: space.id
 
 spin = Spin.create status: 1, title: '1차개발 회고', summary: '처음으로 해본 프로젝트에 대해 회고해봅시다.', project_id: project.id, user_id: user_1.id
-section = Section.create title: '좋았던 점', position: 0, spin_id: spin.id
-section = Section.create title: '안좋았 점', position: 1, spin_id: spin.id
-
+section = Section.create title: '좋았던 점', position: 1, spin_id: spin.id
+Section.create title: '안좋았던 점', position: 2, spin_id: spin.id
+Section.create title: '개선했으면 하는 것', position: 3, spin_id: spin.id
 post = Post.create content: '1차 개발이 완료되었습니다. 좋구만유.', user_id: user_1.id, spin_id: spin.id, section_id: section.id, position: 2
 post = Post.create content: '서버개발 재밌었어욤..', user_id: user_2.id, spin_id: spin.id, section_id: section.id, position: 1
