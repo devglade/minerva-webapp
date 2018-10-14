e40ae363fad98185add894b5e4de98eb7768b23a
Jooyeon <wndus85@gmail.com>
2018-10-14 23:29:43 +0900

    포스트 등록하면 이전 메시지 계속 남아있던 문제 수정.

--- app/javascript/components/section_list.vue
+++ app/javascript/components/section_list.vue
@@ -51,7 +51,7 @@
                     data: data,
                     dataType: "json",
                     success: (data) => {
-                        this.message = ""
+                        this.messages = ""
                         this.$nextTick(() => {
                             this.$refs.message.focus()
                         })
