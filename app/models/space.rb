c4276bf6680c39ca57db8309e8baa9e37f177d09
Jooyeon <wndus85@gmail.com>
2018-12-09 16:56:27 +0900

    공간만들때 공간 만든사람은 자동으로 SpaceMember에 추가됨.

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,12 +1,19 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :projects
-  has_many :space_members
+  has_many :space_members, dependent: :destroy
+
   scope :mine, ->(user_id) {where(user_id: user_id).order('created_at DESC')}
   scope :opend, -> {where("is_public =?", true)}
   validates_format_of :url, with: /[A-Za-z0-9]+/, message: "URL은 영어와 숫자만 허용됩니다", on: :create
 
+
   def updatable_by?(current_user)
     current_user.id == user_id
   end
+
+  after_create do |space|
+    SpaceMember.create(space_id: space.id, user_id: space.user_id)
+  end
+
 end
