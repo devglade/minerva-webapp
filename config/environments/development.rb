6ab0e6b8b808c1200fb096da1457b2bc9b20439b
WiseBell <neoswirl@gmail.com>
2019-02-19 19:01:04 +0900

    환경변수 변경

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,16 +56,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #Add by WiseBell (2017.08.25)
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.default_url_options = {host: ENV['HOST_ADDRESS'], port: 3000}
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: '587',
-      enable_starttls_auto: true,
-      user_name: ENV['GMAIL_USERNAME'],
-      password: ENV['GMAIL_PASSWORD'],
-      authentication: 'plain',
-      domain: 'minerva.devglade.com'
+    address: ENV['SMTP_ADDRESS'],
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: ENV['EMAIL_USER'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    domain: ENV['DOMAIN']
   }
 end
