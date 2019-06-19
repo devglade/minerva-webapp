1578d8ece504b74ed039b0bcf79fa1e96b23ab51
Jooyeon <wndus85@gmail.com>
2019-06-19 22:09:16 +0900

    초대하기 이메일 미리보기 일단 주석.

--- test/mailers/previews/invitation_mailer_preview.rb
+++ test/mailers/previews/invitation_mailer_preview.rb
@@ -2,8 +2,8 @@
 class InvitationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/invitation_mailer/new_user_space_invite
-  def invite_space.html
-    InvitationMailer.invite_space.html
-  end
+  # def invite_space
+  #   InvitationMailer.invite_space.html
+  # end
 
 end
