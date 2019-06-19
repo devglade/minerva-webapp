5614ba39bf9665d7acf2f0617c282e643bd279af
Jooyeon <wndus85@gmail.com>
2019-06-19 21:12:58 +0900

    개발버전에서는 url 옵션을 'localhost'로.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {host: ENV['HOST_ADDRESS'], port: 3000}
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: '587',
