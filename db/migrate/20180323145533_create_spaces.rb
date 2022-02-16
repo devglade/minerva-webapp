80ca788aa6cac67c7f54c658ceee43c210ddb0a8
HyunJongNoh <neoswirl@gmail.com>
2022-02-16 14:47:50 +0900

    Update 20180323145533_create_spaces.rb
    user_id 가 없는 오류

--- db/migrate/20180323145533_create_spaces.rb
+++ db/migrate/20180323145533_create_spaces.rb
@@ -5,7 +5,7 @@ class CreateSpaces < ActiveRecord::Migration[5.1]
       t.string :description, null: true
       t.string :url, null: false
       t.boolean :is_public, null: false, default: true
-
+      t.bigint :user_id, null: false
       t.timestamps
     end
   end
