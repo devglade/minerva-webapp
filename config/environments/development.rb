f716b9349cfe4960088eb23f19d5f432cae51e96
WiseBell <neoswirl@gmail.com>
2022-02-17 08:59:28 +0900

    환경변수 사용 복원

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   #Add by WiseBell (2017.08.25)
   #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
   config.action_mailer.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,13 +102,14 @@ Rails.application.configure do
   config.action_cable.url = 'ws://' + ENV['DOMAIN'] + '/cable'
   config.action_cable.allowed_request_origins = [ /(?:^(http|https):\/\/)?(?:([^.]+)\.)?#{ENV['DOMAIN']}/ ]
 
+  #config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {host:  ENV['DOMAIN'], port: 80}
   config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
+      address: ENV['SMTP_ADDRESS'],
       port: '587',
       authentication: :plain,
-      user_name: 'apikey',
+      user_name: ENV['EMAIL_USER'],
       password: ENV['EMAIL_PASSWORD'],
       domain: ENV['DOMAIN'],
       enable_starttls_auto: true
